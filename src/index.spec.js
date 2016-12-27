b41baf0ad8af12da47b2c3626aa73964414c5048
Pradel Léo <pradel.leo@gmail.com>
2016-12-27 12:08:30 +0100

    Setup jest

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,15 +1,8 @@
-/* eslint-disable no-unused-expressions */
-import chai, { expect } from 'chai';
-import sinonChai from 'sinon-chai';
-import chaiAsPromised from 'chai-as-promised';
 import nock from 'nock';
 import 'isomorphic-fetch';
 import client from './index';
 
 const PATH = 'http://127.0.0.1';
-
-chai.use(sinonChai);
-chai.use(chaiAsPromised);
 
 client.config({
   path: PATH,
@@ -25,7 +18,7 @@ describe('client', () => {
     });
 
     return client.login({}).then((res) => {
-      expect(res).to.eql({
+      expect(res).toEqual({
         state: 200,
         message: 'Mocked response',
       });

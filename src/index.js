30ad09af51c03ccef84df0f60a0c1bb045799c4c
Tim Mikeladze <tim.mikeladze@gmail.com>
2017-01-03 15:41:50 -0800

    Added headers

--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ import { AccountsClient } from '@accounts/accounts';
 const defaultConfig = {
 };
 
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+
 const client = {
   fetch(route, args) {
     return fetch(`${AccountsClient.options().server}${AccountsClient.options().path}/${route}`, {
-      mode: 'no-cors',
+      headers,
       ...args,
     }).then(res => res.json());
   },
   login(body) {
     return this.fetch('login', {
       method: 'POST',
-      body,
+      body: JSON.stringify(body),
     }).then(res => (res.json()));
   },
   createUser(body) {
     return this.fetch('createUser', {
       method: 'POST',
-      body,
+      body: JSON.stringify(body),
     });
   },
   config(config) {

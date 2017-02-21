04edb3bdb547f9d4c30aee9c534745bf5e3cb7d6
David Yahalomi <davidyaha@gmail.com>
2017-02-21 19:19:43 +0200

    * BREAKING CHANGE: @accounts/rest-client now exports the Client class that expects an option object to be supplied on the constructor. * Added flow to client * Added simple test for options object

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,11 +1,31 @@
+// @flow
+
 import AccountsClient from '@accounts/client';
+import type { TransportInterface } from '@accounts/client';
+import type {
+  CreateUserType,
+  PasswordLoginUserType,
+  LoginReturnType,
+} from '@accounts/common';
+
+export type OptionsType = {
+  apiHost: string,
+  rootPath: string
+};
 
 const headers = new Headers();
 headers.append('Content-Type', 'application/json');
 
-const client = {
-  async fetch(route, args) {
-    const res = await fetch(`${AccountsClient.options().server}${AccountsClient.options().path}/${route}`, {
+export default class Client {
+  constructor(options: OptionsType) {
+    // Enforce flow interface on current class
+    // eslint-disable-next-line no-unused-expressions
+    (this: TransportInterface);
+    this.options = options;
+  }
+
+  async fetch(route: string, args: Object): Promise<any> {
+    const res = await fetch(`${this.options.apiHost}${this.options.rootPath}/${route}`, {
       headers,
       ...args,
     });
@@ -19,8 +39,9 @@ const client = {
     } else {
       throw new Error('Server did not return a response');
     }
-  },
-  loginWithPassword(user, password) {
+  }
+
+  loginWithPassword(user: PasswordLoginUserType, password: string): Promise<LoginReturnType> {
     return this.fetch('loginWithPassword', {
       method: 'POST',
       body: JSON.stringify({
@@ -28,14 +49,16 @@ const client = {
         password,
       }),
     });
-  },
-  createUser(user) {
+  }
+
+  async createUser(user: CreateUserType): Promise<string> {
     return this.fetch('createUser', {
       method: 'POST',
       body: JSON.stringify({ user }),
     });
-  },
-  refreshTokens(accessToken, refreshToken) {
+  }
+
+  refreshTokens(accessToken: string, refreshToken: string): Promise<LoginReturnType> {
     return this.fetch('refreshTokens', {
       method: 'POST',
       body: JSON.stringify({
@@ -43,24 +66,27 @@ const client = {
         refreshToken,
       }),
     });
-  },
-  logout(accessToken) {
+  }
+
+  logout(accessToken: string): Promise<void> {
     return this.fetch('logout', {
       method: 'POST',
       body: JSON.stringify({
         accessToken,
       }),
     });
-  },
-  verifyEmail(token) {
+  }
+
+  verifyEmail(token: string): Promise<void> {
     return this.fetch('verifyEmail', {
       method: 'POST',
       body: JSON.stringify({
         token,
       }),
     });
-  },
-  resetPassword(token, newPassword) {
+  }
+
+  resetPassword(token: string, newPassword: string): Promise<void> {
     return this.fetch('resetPassword', {
       method: 'POST',
       body: JSON.stringify({
@@ -68,8 +94,9 @@ const client = {
         newPassword,
       }),
     });
-  },
-  sendVerificationEmail(userId, email) {
+  }
+
+  sendVerificationEmail(userId: string, email: string): Promise<void> {
     return this.fetch('sendVerificationEmail', {
       method: 'POST',
       body: JSON.stringify({
@@ -77,8 +104,9 @@ const client = {
         email,
       }),
     });
-  },
-  sendResetPasswordEmail(userId, email) {
+  }
+
+  sendResetPasswordEmail(userId: string, email: string): Promise<void> {
     return this.fetch('sendResetPasswordEmail', {
       method: 'POST',
       body: JSON.stringify({
@@ -86,12 +114,12 @@ const client = {
         email,
       }),
     });
-  },
-};
+  }
 
-export default client;
+  options: OptionsType;
+}
 
-const authFetch = async (path, request) => {
+const authFetch = async(path: string, request: Object) => {
   await AccountsClient.resumeSession();
   const tokens = AccountsClient.tokens();
   const headers = new Headers({ // eslint-disable-line no-shadow
